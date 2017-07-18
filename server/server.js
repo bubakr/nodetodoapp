5fccc5e14bfc9f3b6befcf45c82c8688bcdd9a16
Ahmad Abubakr <ahmad@tqniat.com>
2017-07-18 13:08:52 +0200

    change todo url to todos:id

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get('/todos', (req, res)=>{
 });
 
 
-app.get('/todo/:id', (req, res) => {
+app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)){
         res.status(404).send();
